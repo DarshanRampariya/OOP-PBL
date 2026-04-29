2ba17cbf22fe829ec17c6cb1563c044ec69f978b
DarshanRampariya <darshanrampariya12@gmail.com>
2026-04-29 20:33:18 +0530

    pr1.java

--- set 1/pr1.java
+++ set 1/pr1.java
@@ -1,1 +1,17 @@
-dasdasddfs
+import java.util.*;
+
+public class practical_1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter distance in meters: ");
+        double meters = sc.nextDouble();
+
+        double feet = meters * 3.28084;
+
+        System.out.printf("Distance in feet = %.2f", feet);
+
+        sc.close();
+    }
+}
